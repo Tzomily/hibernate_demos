ef84682c751fe428dd9816775c9631fa9a024189
Tzomily <jomilyanv@gmail.com>
2021-01-17 16:04:17 +0200

    Add all projects so far

---
+++ hb-02-one-to-one-bi/src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,23 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String password = "hbstudent";
+
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			Connection myConnection = DriverManager.getConnection(jdbcUrl, user, password);
+			System.out.println("Conncetion successful!");
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
---
+++ spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
@@ -0,0 +1,31 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<!DOCTYPE>
+<html>
+
+<head>
+<title>Student Confirmation</title>
+</head>
+
+
+<body>
+
+	The student is confirmed: ${student.firstName } ${student.lastName }
+	<br>
+	<br> Country: ${student.country }
+	<br>
+	<br> Favourite Language: ${student.favouriteLanguage}
+	<br>
+	<br> Operating systems:
+	<ul>
+		<c:forEach var="temp" items="${student.operatingSystems}">
+
+			<li>${temp}</li>
+
+		</c:forEach>
+	</ul>
+
+</body>
+
+
+</html>
